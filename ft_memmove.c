4e4e247f43e446f9424dc1e67706e4ebc8196786
Ignacio Ortego De La Mata <iortego-@c3r8s4.42madrid.com>
2022-06-16 21:07:02 +0200

    memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: iortego- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 20:22:09 by iortego-          #+#    #+#             */
-/*   Updated: 2022/06/16 20:22:09 by iortego-         ###   ########.fr       */
+/*   Updated: 2022/06/16 21:06:35 by iortego-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,14 @@
 
 void	*ft_memmove(void	*dst, const void	*src, size_t	len)
 {
-		while (0 < len--)
-			dest[len] = src[len];
-		return (dest);
+	size_t	count;
+
+	count = 0;
+	while (scr + count != dest)
+		count++;
+	if (scr + count == dest)
+		return dest;
+	while (0 < len--)
+		dest[len] = src[len];
+	return (dest);
 }
