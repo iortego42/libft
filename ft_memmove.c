9311baa1799971878ba010e7d2787f4a1a09dc4d
Ignacio Ortego De La Mata <iortego-@c2r4s2.42madrid.com>
2022-06-17 20:42:27 +0200

    memmoveActualizacion

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,26 +6,24 @@
 /*   By: iortego- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 20:22:09 by iortego-          #+#    #+#             */
-/*   Updated: 2022/06/16 21:06:35 by iortego-         ###   ########.fr       */
+/*   Updated: 2022/06/17 20:41:38 by iortego-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdint.h>
 
 void	*ft_memmove(void	*dest, const void	*src, size_t	len)
 {
-	size_t	count;
-	void	*ptr;
-	int	a;
-// hay que mirar como castear punteros y repasar los voids
-	ptr = dest;
-	count = 0;
-	while (src + count != ptr)
-		count++;
-	if (src + count == ptr)
-		return (ptr);
-	while (0 < len--)
-		(long int)ptr[len] = src[len];
-	return (ptr);
+	if (src < dest)
+		while (0 < len--)
+			*(unsigned char *)(dest + len) = *(const unsigned char *)(src + len);
+	else
+	{
+		size_t	count;
+		
+		count = 0;
+		while (count < len)
+			((unsigned char *)dest)[count] = ((const unsigned char *)src)[count++];
+	}
+	return (dest);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: iortego- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 20:47:37 by iortego-          #+#    #+#             */
-/*   Updated: 2022/06/16 20:48:10 by iortego-         ###   ########.fr       */
+/*   Updated: 2022/06/17 20:41:48 by iortego-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,19 @@
 
 #include <stdlib.h>
 
-int ft_isalpha(char	c);
-int ft_isdigit(char	c);
-int ft_isalnum(char	c);
-int ft_isascii(char	c);
-int ft_isprint(char	c);
-int	ft_atoi(char	*str);
-int	ft_isspace(char	c);
+int		ft_isalpha(char	c);
+int		ft_isdigit(char	c);
+int		ft_isalnum(char	c);
+int		ft_isascii(char	c);
+int		ft_isprint(char	c);
+int		ft_atoi(char	*str);
 void	ft_bzero();
-void	ft_calloc();
-void	ft_memchr();
+void	*ft_calloc(size_t	count, size_t	size);
+void	*ft_memchr(const void	*s, int	c, size_t	n);
 void	ft_memcmp();
 void	ft_memcpy();
-void	ft_memmove();
-void	ft_memset();
+void	*ft_memmove(void	*dest, const void	*src, size_t	len);
+void	*ft_memset(void	*b, int	c, size_t len);
 void	ft_strchtr();
 void	ft_strdutp();
 void	ft_strlcat();
@@ -39,4 +38,6 @@ void	ft_strnstr();
 void	ft_strrchr();
 void	ft_tolower();
 void	ft_toupper();
+//--MIS FUNCIONES--
+int		ft_isspace(char	c);
 #endif
