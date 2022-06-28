97e629df175e457ae4f8bac31978fa2a8370ea62
Ignacio Ortego De La Mata <iortego-@c3r4s3.42madrid.com>
2022-06-28 20:01:52 +0200

    nuevos archivos

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: iortego- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 20:47:37 by iortego-          #+#    #+#             */
-/*   Updated: 2022/06/27 15:10:25 by iortego-         ###   ########.fr       */
+/*   Updated: 2022/06/28 19:30:18 by iortego-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,19 @@ char	*ft_strchr(const char	*s, int	c);
 int	ft_tolower(int	c);
 int	ft_toupper(int	c);
 
+/*--FUNCIONES ADICIONALES*/
+
+char	*ft_substr(char const	*s, unsigned int	start, size_t	len);
+char	*ft_strjoin(char const	*s1, char const	*s2);
+char	*ft_strtrim(char const	*s1, char const	*set);
+char	**ft_split(char const	*s, char	c);
+char	*ft_itoa(int	n);
+char	*ft_strmapi(char const	*s, char	(*f)(unsigned int, char));
+void	ft_striteri(char	*s, void	(*f)(unsigned int, char*));
+void	ft_putchar_fd(char	c, int	fd);
+void	ft_putstr_fd(char	*s, int	fd);
+void	ft_putendl_fd(char	*s, int fd);
+void	ft_putnbr_fd(int	a, int fd);
 /*--MIS FUNCIONES--*/
 
 int		ft_isspace(char	c);
