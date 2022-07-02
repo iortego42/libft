f0e2287fea4291dddc75eeb06873cc3551ab7868
Ignacio Ortego De La Mata <iortego-@c3r13s3.42madrid.com>
2022-07-02 17:52:02 +0200

    delete

--- ft_matrix_delete.c
+++ ft_matrix_delete.c
@@ -6,7 +6,7 @@
 /*   By: iortego- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 16:29:19 by iortego-          #+#    #+#             */
-/*   Updated: 2022/07/02 16:56:04 by iortego-         ###   ########.fr       */
+/*   Updated: 2022/07/02 17:51:43 by iortego-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,13 @@ void	ft_matrix_delete(void	*matrix, int	dimension, long int *count)
 {
 	int	free_count;
 
+	free_count = 0;
 	while (dimension-- > 0)
-	{
-		while (count[dimension] > 0 && dimension > 1)
-			ft_all_free(*(matrix + --count[dimension]), dimension, count);
+	{	
+		while (count[dimension] > free_count && dimension > 0)
+			ft_all_free(*(matrix + free_count++), dimension, count);	
 		free_count = 0;
-		while (count[dimension] > free_count && dimension > 1)
-		{
+		while (count[dimension] > free_count)
 			free(matrix + free_count++);
-		}
 	}
 }
