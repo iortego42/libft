a98417966fdcbaaab710fd07a6843f9d18ef3e36
Ignacio Ortego De La Mata <iortego-@c3r2s4.42madrid.com>
2022-07-02 14:13:10 +0200

    actualizacion y prototipo del allfree

--- ft_split.c
+++ ft_split.c
@@ -6,36 +6,45 @@
 /*   By: iortego- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 11:49:43 by iortego-          #+#    #+#             */
-/*   Updated: 2022/06/30 13:12:01 by iortego-         ###   ########.fr       */
+/*   Updated: 2022/07/02 14:12:17 by iortego-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_how_many_word(char const	*s, char	centinel)
+int	ft_how_many_word(char const	*s, char	sentinel)
 {
 	int	count;
 
 	count = 0;
 	while (*(s) != 0)
-		if (*(s++) != centinel && (*s == centinel || *s == 0))
+		if (*(s++) != sentinel && (*s == sentinel || *s == 0))
 			count++;
 	return (count);
 }
-int	ft_how_many_char(char const *s, char	 centinel)
+int	ft_how_many_char(char const *s, char	 sentinel)
 {
 	int	count;
 
 	count = 0;
-	while (*s != centinel && *(s++) != 0)
+	while (*s != sentinel && *(s++) != 0)
 		count++;
 	return (count);
 }
-/* En desarrollo, recibe cualquier tipo de matriz y en funcion del numero de dimensiones que tenga la libera entera
-void	ft_all_free(void	ptr, long long int	count[10])
+ 
+void	ft_all_free(void	*matrix, int	dimension, long long int	*count)
 {
-	
-}*/
+	int	free_count;
+
+	while (dimension-- > 0)
+	{
+		if (dimension != 1)
+			ft_all_free(*matrix, dimension, count);
+		free_count = 0;
+		while (count[dimension] > free_count)
+			free(matrix + free_count++);
+	}	
+}
 char	**ft_split(char const	*s, char	c)
 {
 	char	**list;
@@ -43,8 +52,8 @@ char	**ft_split(char const	*s, char	c)
 	int		count;
 
 	count = 0;
-	words = ft_how_many_word(s, c);
-	list = ft_calloc(words + 1, sizeof(char	*));
+	words = 0;
+	list = ft_calloc(ft_how_many_word(s, c) + 1, sizeof(char	*));
 	if (list == NULL)
 		return (NULL);	
 	while (s != 0)
@@ -52,11 +61,11 @@ char	**ft_split(char const	*s, char	c)
 		while (*s == c)
 			s++;
 		count = ft_how_many_char(s, c);
-		*list = ft_calloc(count + 1, sizeof(char));
-		if (*list == NULL)
+		*(list + words) = ft_calloc(count + 1, sizeof(char));
+		if (*(list + words) == NULL)
 			return (NULL);
 		s += count;
-		ft_strlcpy(*(list++), s, count + 1);
+		ft_strlcpy(*(list + words++), s, count + 1);
 	}
-	return (list - words);
+	return (list);
 }
