09b387bed67811b82c6015a17bf60854dae90c81
Ignacio Ortego De La Mata <iortego-@c3r4s1.42madrid.com>
2022-07-03 15:15:57 +0200

    actualizacion de matrix deleted, ahora funciona con NULL terminated

--- ft_matrix_delete.c
+++ ft_matrix_delete.c
@@ -6,31 +6,28 @@
 /*   By: iortego- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 16:29:19 by iortego-          #+#    #+#             */
-/*   Updated: 2022/07/02 17:51:43 by iortego-         ###   ########.fr       */
+/*   Updated: 2022/07/03 15:09:53 by iortego-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_matrix_delete(void	**matrix, int	dimension, long int	*count)
+void	ft_matrix_delete(void	**matrix, int	dimension)
 {
-	ft_matrix_free(matrix, dimension, count);
+	ft_matrix_free(matrix, dimension);
 	matrix = NULL;
 }
-void	ft_matrix_free(void	**matrix, int	dimension, long int	*count)
+void	ft_matrix_free(void	**matrix, int	dimension)
 {
 	int	free_count;
 
 	free_count = 0;
 	while (dimension-- > 0)
 	{	
-		while (count[dimension] > free_count && dimension > 0)
-			ft_matrix_delete(*(matrix + free_count++), dimension, count);	
+		while (*(matrix + free_count) != NULL && dimension > 0)
+			ft_matrix_delete(*(matrix + free_count++), dimension);	
 		free_count = 0;
 		while (count[dimension] > free_count)
-		{
 			free(matrix + free_count++);
-			matrix = NULL;
-		}
 	}
 }
