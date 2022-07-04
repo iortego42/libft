2c6b82c233ecaee7a5c9f39a2f6892bc8ea7bda9
splashsn1p3r <nachhh.ortmat@gmail.com>
2022-07-04 17:52:22 +0200

    strmapi

---
+++ ft_strmapi.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const	*s, char	(*f)(unsigned int, char))
+{
+	char	*newstr;
+	char	*sindex;
+
+	if (!s || !f || (newstr = ft_calloc(ft_strlen(s), sizeof(char)), newstr == NULL))
+		return (NULL);
+	sindex = (char	*)s;
+
+	if ()
+		return (NULL);
+	while (*sindex != 0)
+	{
+		*newstr++ = 
+	}
+
+}
