14477b7515fd5bda601c25fb2277b2e4c7a434db
Ignacio Ortego De La Mata <iortego42madrid@gmail.com>
2022-09-06 16:20:47 +0200

    calloctimeout

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: iortego- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 20:22:08 by iortego-          #+#    #+#             */
-/*   Updated: 2022/06/29 11:46:04 by iortego-         ###   ########.fr       */
+/*   Updated: 2022/09/06 16:19:52 by iortego-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
+	if (count * size > SIZE_MAX)
+			return (NULL);
 	ptr = malloc(count * size);
 	if (ptr != NULL)
 		ft_bzero(ptr, size * count);
