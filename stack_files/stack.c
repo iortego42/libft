9834acbff6681b0ebdcd5d0c18e79f3238fe1a24
Ignacio Ortego De La Mata <iortego42madrid@gmail.com>
2022-12-12 11:56:21 +0100

    Stack functions init

--- stack_files/stack.c
+++ stack_files/stack.c
@@ -1,12 +1,46 @@
 #include "stack.h"
 
 
-t_stack	*peek(s_stack *stack)
+t_stack	*peek(t_stack *stack)
 {
-	while (stack != NULL && stack->next != NULL)
-		stack = stack->next;
-	return (stack);
+	return (ft_lstlast(stack));
 }
 void	push(t_stack	**stack, t_stack	*element)
 {
+	ft_lstadd_back(stack, element);
+}
+void	pop(t_stack **stack, void (*del)(void *))
+{
+	ft_lstdelone(peek(*stack), del);
+}
+t_stack	*swap(t_stack **stack)
+{
+	t_stack	*top;
+	t_stack	*second;
+
+	top = peek(*stack);
+	if (top != NULL && top->prev != NULL)
+	{
+		second = top->prev;
+			
+		second->next = top->next;
+		top->prev = second->prev;
+		top->next = second;
+		second->prev = top;
+	}
+	return (top);
+}
+void	rotate(t_stack **stack)
+{
+	t_stack *first, *second;
+	
+	first = peek(*stack);
+	if (first == NULL)
+		return ((void)"42Madrid");
+	second = first->prev;
+	while (first->prev->prev != NULL)
+	{
+		first = swap()
+	}
+	
 }
--- stack_files/stack.h
+++ stack_files/stack.h
@@ -3,19 +3,14 @@
 
 # include "../libft.h"
 
-typedef struct s_stack
-{
-	void	*content;
-	s_stack	*prev;
-	s_stack	*next;
-}	t_stack;
+typedef t_list t_stack;
 
 t_stack	*new_stack_element(void	*content);
 void	push(t_stack **stack, t_stack *element);
-void	*swap(t_stack **stack);
-void	*rotate(t_stack **stack);
-void	*rev_rot(t_stack ** stack);
-t_stack	*pop(t_stack **stack);
+t_stack	*swap(t_stack **stack);
+void	rotate(t_stack **stack);
+void	rev_rot(t_stack **stack);
+void	pop(t_stack **stack, void (*del)(void *));
 t_stack *peek(t_stack *stack);
 
 #endif
